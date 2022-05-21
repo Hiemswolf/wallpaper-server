08501ba026bff70e0e1798c67ec7ccd996e0d012
Hiemswolf <claytonkruse08@gmail.com>
2022-05-21 14:16:48 -0500

    master

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
+
+const dir = path.join(__dirname, '../public/wallpapers');
+const fileNames = fs.readdirSync(dir);
+
+
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/wallpapers/Pack.jpg'));
+	res.sendFile(path.join(dir, fileNames[Math.floor(Math.random() * fileNames.length)]));
 });
 
 export default app;
