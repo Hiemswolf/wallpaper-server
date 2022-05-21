2dd982dd41d9380a70cb6df9ca55c68470fbdc93
Hiemswolf <claytonkruse08@gmail.com>
2022-05-21 14:33:06 -0500

    another

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,25 @@ app.use(express.static(path.join(__dirname, '../public')));
 const dir = path.join(__dirname, '../public/wallpapers');
 const fileNames = fs.readdirSync(dir);
 
+interface Wallpaper {
+	name: string,
+	title: string,
+	url: string
+}
+
+let wallpapers: Array<Wallpaper> = [];
+
+for (const fileName of fileNames) {
+	wallpapers = [...wallpapers, {
+		name: fileName,
+		title: fileName.slice(0, -4),
+		url: `/wallpapers/${fileName}`
+	}];
+}
+
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(dir, fileNames[Math.floor(Math.random() * fileNames.length)]));
+	res.send(wallpapers[Math.floor(Math.random() * wallpapers.length)]);
 });
 
 export default app;
